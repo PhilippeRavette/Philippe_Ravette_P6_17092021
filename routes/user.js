866ffefb33009ac7a388cad638f7e0ec2743cf61
philippe ravette <virauteuil1@gmail.com>
2021-09-24 16:14:31 +0200

    modif gitignore

--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,6 @@ const router = express.Router(); //Permet de charger le middleware au niveau du 
 const userCtrl = require("../controllers/user"); //Appel de la logique métier de nos routes
 const authentificationLimiter = require("../middleware/authentificationLimiter"); //Appel du middleware authenticationLimiter
 
-const rateLimit = require("express-rate-limit");
-const rateLimiter = rateLimit({
-    windowMs: 5 * 60 * 1000,
-    max: 5, //
-    message: " Trop de tentatives échouées, réessayez dans 5 minutes",
-});
-
 router.post("/signup", userCtrl.signup); //Création d'un nouvel utilisateur
 router.post("/login", authentificationLimiter, userCtrl.login); //Login d'un utilisateur existant
 
